77a0fc27e60b34d5d51bd7cf90a322e598b38d5c
BohdanZyshchyk <bohdan.zyshchuk@zagravagames.com>
2021-02-07 23:10:37 +0200

    first commit

---
+++ src/main/java/app/web/HomeController.java
@@ -0,0 +1,109 @@
+package app.web;
+
+import java.util.*;
+import app.entities.User;
+import app.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public HomeController(UserRepository userRepository,
+                          PasswordEncoder passwordEncoder)
+    {
+        this.userRepository=userRepository;
+        this.passwordEncoder=passwordEncoder;
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
+
+//        User user = new User();
+//        user.setName("Валера");
+//        user.setEmail("jon@gmail.com");
+//        String pass = passwordEncoder.encode("123456");
+//        user.setPassword(pass);
+//        userRepository.save(user);
+        List<User> users = userRepository.findAll();
+        model.addAttribute("users", users);
+        return "index";
+    }
+
+    @GetMapping("/contact")
+    public String contactPage() {
+        return "contact";
+    }
+
+    @GetMapping("/about")
+    public String aboutPage() {
+        return "about";
+    }
+
+    @GetMapping("/create")
+    public String showSignUpForm(User user) {
+        return "create";
+    }
+
+    @PostMapping("/create")
+    public String addUser(@Valid User user, BindingResult result, Model model) {
+        User findUser = userRepository.findByEmail(user.getEmail());
+        if(findUser != null)
+        {
+            model.addAttribute("error", "Email already exist");
+            return "create";
+        }
+        if (result.hasErrors()) {
+            return "create";
+        }
+
+        userRepository.save(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editUser(@PathVariable("id") Long id, Model model) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        model.addAttribute("user", user);
+        return "edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String updateUser(@PathVariable("id") Long id, User user,
+                             BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            user.setId(id);
+            return "edit";
+        }
+        userRepository.save(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id, Model model) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        userRepository.delete(user);
+        return "redirect:/";
+    }
+}
